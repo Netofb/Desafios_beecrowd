673408d3c91c7ba4c8b7f8564bde09444584579e
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-30 06:30:02 -0300

    desafio 011

--- problems/1011.js
+++ problems/1011.js
@@ -1,10 +1,10 @@
 
 export function problem(lines){
 
-    const [R] = lines[0];
+    const R = lines[0];
     const pi = 3.14159;
 
-    const total = (4/3) * pi * Number(R) * Number(R) * Number(R);
+    const total = (4/3.0) * pi * (Number(R) * Number(R) * Number(R));
 
     console.log(`VOLUME = ${total.toFixed(3)}`);
 }
