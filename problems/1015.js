85ad1dbaffb938f7ea7b810e14874002710ef807
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-06 06:32:33 -0300

    desafio 015

--- problems/1015.js
+++ problems/1015.js
@@ -1,8 +1,11 @@
 
 export function problem(lines){
 
-    const [A, B, C] = lines[0, 1, 2].split(' ').map(Number);
-    console.log(A, B, C)
+    const [X1, Y1] = lines[0].split(' ').map(Number);
+    const [X2, Y2] = lines[1].split(' ').map(Number);
 
-
+    const p1 = (Number(X2) - Number(X1)) * (Number(X2) - Number(X1));
+    const p2 = (Number(Y2) - Number(Y1)) * (Number(Y2) - Number(Y1));
+    const total = Math.sqrt((p1) + (p2));
+    console.log(`${total.toFixed(4)}`)
 }
