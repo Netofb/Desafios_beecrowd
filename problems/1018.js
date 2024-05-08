eadf322f0568563453964fa3659fc38c710ac42a
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-08 06:57:13 -0300

    desafio 018

--- problems/1018.js
+++ problems/1018.js
@@ -1,7 +1,17 @@
 export function problem(lines){
-    const [valor] = lines(0).map(Number);
 
-
-
+    const valor = parseInt(lines[0]);
+    console.log(valor);
     
+    let resto = valor;
+    
+    const notas = [100, 50, 20, 10, 5, 2, 1];
+    
+    for (let nota of notas) {
+        const quantidade = parseInt(resto / nota);
+        console.log(`${quantidade} nota(s) de R$ ${nota},00`);
+        resto = resto % nota;
+    }
+    
+   
 }
