a5659bac6c3d7b313c56472a89a4b341cac338f5
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-15 07:06:13 -0300

    desafio 20

---
+++ problems/1020.js
@@ -0,0 +1,33 @@
+
+
+export function problem(lines){
+
+    const valor = parseInt(lines[0]);
+    const valores = [365, 30, 1];
+
+    let resto = valor;
+
+
+    let quantidade1 = parseInt(resto / valores[0]);
+    console.log(`${quantidade1} ano(s)`);
+    resto = parseInt(resto % valores);
+    let quantidade2 = parseInt(resto / valores[1]);
+    console.log(`${quantidade2} mes(es)`);
+    resto = parseInt(resto % valores);
+    let quantidade3 = parseInt(resto / valores[3]);
+    console.log(`${quantidade3} dia(s)`);
+
+    
+}
+
+
+
+
+
+
+
+
+	
+// 1 ano(s)
+// 1 mes(es)
+// 5 dia(s)
